288617511c9225769ecb5b171d67125e6c880b3b
m-ox <maudlin.oxalis@gmail.com>
2023-11-03 00:31:06 -0600

    added dummy patient data json and small path variable mapping for fun

--- src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
+++ src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
@@ -2,6 +2,8 @@ package com.mox.springfhiringestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,19 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringFhirIngestionApplication {
 
-	// @RequestMapping("/")
-  //   public String home() {
-  //       return "Entry point for Springboot Fhir Ingestion";
-  //   }
+	@RequestMapping("/")
+    public String home() {
+        return "Entry point for Springboot Fhir Ingestion";
+    }
+
+	@GetMapping("/get-field/{patientData}")
+		public String getField(@PathVariable String patientData) {
+			return patientData;
+		}
 
 	public static void main(String[] args) {
-		System.out.print("Hello, Maud");
+		SpringApplication.run(SpringFhirIngestionApplication.class, args);
 	}
-
-	// @RequestMapping("/get-field")
-
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(SpringFhirIngestionApplication.class, args);
-	// }
 
 }
