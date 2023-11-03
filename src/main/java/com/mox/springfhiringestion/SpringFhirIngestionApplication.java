de2c273d64bfb24cd05a05295e23bed05d4d20c3
m-ox <maudlin.oxalis@gmail.com>
2023-11-02 23:51:48 -0600

    holding onto this while I'm reading

--- src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
+++ src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
@@ -14,6 +14,8 @@ public class SpringFhirIngestionApplication {
         return "Hello World";
     }
 
+	// @RequestMapping("/get-field")
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFhirIngestionApplication.class, args);
 	}
