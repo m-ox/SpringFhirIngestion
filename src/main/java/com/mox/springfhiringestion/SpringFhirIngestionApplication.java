84612cbf3a58f4a0161c3b5546f16e625723d00a
m-ox <maudlin.oxalis@gmail.com>
2023-11-06 11:36:25 -0700

    entry point accepting gender param and returns default gender from a new patient obj

--- src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
+++ src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
@@ -1,11 +1,15 @@
 package com.mox.springfhiringestion;
 
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.uhn.fhir.model.dstu2.resource.Patient;
+import ca.uhn.fhir.model.dstu2.valueset.AdministrativeGenderEnum;
 
 @SpringBootApplication
 @RestController
@@ -16,9 +20,21 @@ public class SpringFhirIngestionApplication {
         return "Entry point for Springboot Fhir Ingestion";
     }
 
-	@GetMapping("/get-field/{patientData}")
-		public String getField(@PathVariable String patientData) {
-			return patientData;
+	@GetMapping("field/{fieldParam}")
+		public String getParam(@PathVariable String fieldParam) {
+			// Create a new Patient resource
+			Patient patient = new Patient();
+
+			if (fieldParam.equals("gender")) {
+				patient.setGender(AdministrativeGenderEnum.MALE);
+				// return """
+				// 		Patient gender: g%
+				// 		""".formatted(patient.getGender());
+				return patient.getGender();
+			}
+			// will default to id
+			// return patient.toString();
+			return fieldParam;
 		}
 
 	public static void main(String[] args) {
