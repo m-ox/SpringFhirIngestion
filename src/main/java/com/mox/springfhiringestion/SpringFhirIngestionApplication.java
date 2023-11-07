ce931afc932b462eba5b3c0de296338e18bb1820
m-ox <maudlin.oxalis@gmail.com>
2023-11-06 21:50:03 -0700

    created some extra trash endpoints, moved patient example data and updated it to what Eric suggested to use and convert

--- src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
+++ src/main/java/com/mox/springfhiringestion/SpringFhirIngestionApplication.java
@@ -1,11 +1,15 @@
 package com.mox.springfhiringestion;
 
-import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.uhn.fhir.model.dstu2.resource.Patient;
@@ -15,27 +19,29 @@ import ca.uhn.fhir.model.dstu2.valueset.AdministrativeGenderEnum;
 @RestController
 public class SpringFhirIngestionApplication {
 
+	// hello world
 	@RequestMapping("/")
     public String home() {
         return "Entry point for Springboot Fhir Ingestion";
     }
 
-	@GetMapping("field/{fieldParam}")
-		public String getParam(@PathVariable String fieldParam) {
+	// test route: accept id and field parameters (ex. gender)
+	@GetMapping("test/{idParam}/{fieldParam}")
+		public String getParam(@PathVariable String idParam, String fieldParam) {
 			// Create a new Patient resource
 			Patient patient = new Patient();
 
 			if (fieldParam.equals("gender")) {
 				patient.setGender(AdministrativeGenderEnum.MALE);
-				// return """
-				// 		Patient gender: g%
-				// 		""".formatted(patient.getGender());
 				return patient.getGender();
 			}
-			// will default to id
-			// return patient.toString();
-			return fieldParam;
+			return idParam + fieldParam;
 		}
+
+	@PostMapping("/test/buh")
+			public @ResponseBody ResponseEntity<?> findBuh(@RequestBody String body) throws Exception {
+				return new ResponseEntity<String>(body, HttpStatus.OK);
+			}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFhirIngestionApplication.class, args);
